9daf5f729759e2d4367adc8f0fa938dc1a79a1dc
Jailson de Souza <jailson.souza@compasso.com.br>
2020-11-25 23:47:56 -0300

    primary commit

---
+++ alurabank/app/ts/models/Negociacoes.ts
@@ -0,0 +1,17 @@
+class Negociacoes {
+
+    private _negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao: Negociacao) : void {
+
+        this._negociacoes.push(negociacao);
+    }
+
+    paraArray() : Array<Negociacao> {
+
+        return [].concat(this._negociacoes);
+    }
+}
+
+
+// Posso substituir Array<Negociacao> por Negociacao[]
